2248a4cc053e9a29b01413e02503e5bb4d623dd8
재권 <diwo02@naver.com>
2024-06-23 19:02:46 +0900

    메뉴수정부분 실시간업데이트 처리완료

--- RoadProject/src/main/java/com/pk/dto/MenuDto.java
+++ RoadProject/src/main/java/com/pk/dto/MenuDto.java
@@ -1,27 +1,46 @@
 package com.pk.dto;
 
 import java.sql.Timestamp;
-
 import org.springframework.stereotype.Repository;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Repository
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL) // null 필드를 JSON에서 제외
 public class MenuDto {
 	
-	private int id;
-	private int business;
-	private String m_name;
-	private String m_cost;
-	private String m_intro;
-	private String m_code;
-	private String imnum;
-	private Timestamp wdate;
-	private int visible;
-	private String thumbnail;
-	
-	public MenuDto() {
-		
-	}
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("business")
+    private int business;
+
+    @JsonProperty("m_name")
+    private String m_name;
+
+    @JsonProperty("m_cost")
+    private String m_cost;
+
+    @JsonProperty("m_intro")
+    private String m_intro;
+
+    @JsonProperty("m_code")
+    private String m_code;
+
+    @JsonProperty("imnum")
+    private String imnum;
+
+    @JsonProperty("wdate")
+    private Timestamp wdate;
+
+    @JsonProperty("visible")
+    private int visible;
+
+    @JsonProperty("thumbnail")
+    private String thumbnail;
+    
+    public MenuDto() {
+    }
 }
